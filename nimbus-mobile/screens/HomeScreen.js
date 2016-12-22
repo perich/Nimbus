99bcc5c238d145cc52a700b8443e454be633e9eb
Chris <krizloo@gmail.com>
2016-12-22 12:01:35 -0800

    Initialize react native exponent project Nimbus

---
+++ nimbus-mobile/screens/HomeScreen.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Components } from 'exponent';
+import {
+  Image,
+  Linking,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import { MonoText } from '../components/StyledText';
+
+export default class HomeScreen extends React.Component {
+  static route = {
+    navigationBar: {
+      visible: false,
+    },
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Components.MapView
+        style={{flex: 1}}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
