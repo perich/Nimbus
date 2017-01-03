2010e51ef423aa33dfc216c2401b5110a7d095dd
Chris <krizloo@gmail.com>
2017-01-02 20:26:01 -0800

    Style add pin page

--- nimbus-mobile/screens/AddPinScreen.js
+++ nimbus-mobile/screens/AddPinScreen.js
@@ -3,6 +3,7 @@ import {
   View,
   Text,
   Image,
+  TextInput,
   ScrollView,
   StyleSheet,
   TouchableHighlight,
@@ -53,26 +54,42 @@ export default class AddPinScreen extends React.Component {
     }
   }
 
+  _goBack() {
+    this.props.navigator.pop();
+  }
+
   render() {
     let { image } = this.state;
 
     return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <TouchableOpacity onPress={this._pickImage}>
+      <ScrollView>
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.pickImageContainer} onPress={this._pickImage}>
+            <View>
+              <Text style={styles.pickImageText}>Pick an image from camera roll</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.pickImageContainer} onPress={this._takePhoto}>
+            <View>
+              <Text style={styles.pickImageText}>Take a Photo</Text>
+            </View>
+          </TouchableOpacity>
+
+          <View style={styles.photoContainer}>
+          {image &&
+            <Image source={{uri: image}} style={{width: 200, height: 200}} /> }
+          </View>
+        </View>
+
+        <TextInput style={styles.descriptionBox} multiline={false} numberOfLines={2} onChangeText={(text) => this.setState({text})} placeholder='Enter a description...' value={this.state.description}/>
+        
+        <TouchableOpacity style={styles.pickImageContainer} onPress={this._goBack.bind(this)}>
           <View>
-            <Text>Pick an image from camera roll</Text>
+            <Text style={styles.pickImageText}>Submit</Text>
           </View>
         </TouchableOpacity>
-
-        <TouchableOpacity onPress={this._takePhoto}>
-          <View>
-            <Text>Take a Photo</Text>
-          </View>
-        </TouchableOpacity>
-
-        {image &&
-          <Image source={{uri: image}} style={{width: 200, height: 200}} /> }
-      </View>
+      </ScrollView>
     );
   }
 
@@ -81,10 +98,28 @@ export default class AddPinScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
     paddingTop: 15,
   },
-  addPhotoContainer: {
-    flex: 5,
-    backgroundColor: 'skyblue',
+  pickImageContainer: {
+    backgroundColor: '#2f95dc',
+    height: 50,
+    width: 300,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 5,
+  },
+  pickImageText: {
+    color: 'white',
+  },
+  photoContainer: {
+    width: 200,
+    height: 200,
+    margin: 5,
+  },
+  descriptionBox: {
+    height: 40,
+    margin: 5,
   }
 });
