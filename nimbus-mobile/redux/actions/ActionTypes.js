4fcc4869b8ba1cea9ea7fe0839bb4b2998dc4973
Steven Jing <steven.jing@gmail.com>
2017-01-07 16:32:24 -0800

    Set up redux skeleton

---
+++ nimbus-mobile/redux/actions/ActionTypes.js
@@ -0,0 +1,3 @@
+export const SET_CURRENT_USER = 'SET_CURRENT_USER';
+export const SIGN_IN = 'SIGN_IN';
+export const SIGN_OUT = 'SIGN_OUT';
---
+++ nimbus-mobile/redux/actions/index.js
@@ -0,0 +1,5 @@
+import * as UserActions from './UserActions.js';
+
+export const ActionCreators = Object.assign({},
+  UserActions,
+);
